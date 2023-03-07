052a9eff683e4acfadf085b34013a32f9336f1b5
Shreyash Gupta <sg973@cornell.edu>
2023-03-07 14:08:40 -0500

    update build for gh-pages

--- src/Pages/Product.js
+++ src/Pages/Product.js
@@ -1,11 +1,22 @@
+import React from "react";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
+import { Unity, useUnityContext } from "react-unity-webgl";
+import './Product.css';
 
 function Product () {
+
+    const { unityProvider } = useUnityContext({
+        loaderUrl: "../../build/UnityWebApp.loader.js",
+        dataUrl: "../../build/UnityWebApp.data",
+        frameworkUrl: "../../build/UnityWebApp.framework.js",
+        codeUrl: "../../build/UnityWebApp.wasm",
+    });
+
     return (
         <Container className="p-5">
             <Row className="p-5">
-                <h1>WebGL here</h1>
+                <Unity unityProvider={unityProvider} className="unity" />
             </Row>
         </Container>
     );
