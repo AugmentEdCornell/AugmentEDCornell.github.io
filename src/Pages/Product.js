55ec39deb883b26bd91675725505851dba18def0
Parthit Patel <pbp38@cornell.edu>
2023-03-21 17:47:26 -0400

    WebGL Build files

--- src/Pages/Product.js
+++ src/Pages/Product.js
@@ -6,16 +6,23 @@ import './Product.css';
 
 function Product () {
 
-    const { unityProvider } = useUnityContext({
-        loaderUrl: "../../build/UnityWebApp.loader.js",
-        dataUrl: "../../build/UnityWebApp.data",
-        frameworkUrl: "../../build/UnityWebApp.framework.js",
-        codeUrl: "../../build/UnityWebApp.wasm",
+    const { unityProvider, sendMessage  } = useUnityContext({
+        loaderUrl: "../../build/React-rs.loader.js",
+        dataUrl: "../../build/React-rs.data",
+        frameworkUrl: "../../build/React-rs.framework.js",
+        codeUrl: "../../build/React-rs.wasm",
     });
+
+    function handleClickSpawnEnemies() {
+        console.log("Please god please");
+        sendMessage("React-Text", "CallableFunctionReact", "Parthit Cool");
+        console.log("Should be workoingm");
+      }
 
     return (
         <Container className="p-5">
             <Row className="p-5">
+                <button onClick={handleClickSpawnEnemies}>Do something</button>
                 <Unity unityProvider={unityProvider} className="unity" />
             </Row>
         </Container>
