2a986b20ebb92603fefdb1a28f4df926b6ba1733
Shreyash Gupta <sg973@cornell.edu>
2023-05-14 13:51:22 -0400

    Quiz from PPT

--- src/Pages/UploadContentFile.js
+++ src/Pages/UploadContentFile.js
@@ -1,9 +1,13 @@
 import Container from "react-bootstrap/Container";
 import Form from "react-bootstrap/Form"
+import Row from "react-bootstrap/Row"
 import './UploadContentFile.css'
 import axios from 'axios';
+import { useState } from "react";
 
 function UploadContentFile () {
+
+    const [getQuiz, setQuiz] = useState({});
 
     const uploadFile = (event) => {
         const file = event.target.files[0]
@@ -18,6 +22,7 @@ function UploadContentFile () {
             }) 
             .then((response) => {
                 console.log(response);
+                setQuiz(response)
             })
             .catch((error) => {
                 console.log(error);
@@ -26,14 +31,29 @@ function UploadContentFile () {
 
     return (
         <Container fluid>
-            <h1 className="ucHeader">Upload Content File</h1>
-            <Form className="p-5" id="fileForm">
-                <Form.Group controlId="file">
-                    <Form.Label>Content File</Form.Label>
-                    <Form.Control type="file" placeholder="PDF" onChange={uploadFile} accept=".pdf, .pptx, .ppt, .pptm"/>
-                </Form.Group>
-
-            </Form>
+            <Row>
+                <h1 className="ucHeader">Upload Content File</h1>
+                <Form className="p-5" id="fileForm">
+                    <Form.Group controlId="file">
+                        <Form.Label>Content File</Form.Label>
+                        <Form.Control type="file" placeholder="PDF" onChange={uploadFile} accept=".pdf, .pptx, .ppt, .pptm"/>
+                    </Form.Group>
+                </Form>
+            </Row>
+            <Row>
+                <h1 className="qcHeader">Quiz on Content</h1>
+                <div className="mt-3 ms-5">
+                    {getQuiz.status === 200 ? 
+                        <p>
+                            {getQuiz.data}
+                        </p>
+                    :
+                        <p>
+                            Loading...
+                        </p>    
+                    }
+                </div>
+            </Row>
         </Container>
     )
 }
